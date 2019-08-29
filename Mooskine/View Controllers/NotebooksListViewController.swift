f4f7ba17d2d89eb669ad73e180c9b9a774c97403
Ramiz Raja <informramiz@gmail.com>
2019-08-29 07:03:14 +0500

    fix: Edit button state not handled correctly

--- Mooskine/View Controllers/NotebooksListViewController.swift
+++ Mooskine/View Controllers/NotebooksListViewController.swift
@@ -170,6 +170,7 @@ extension NotebooksListViewController: NSFetchedResultsControllerDelegate {
     
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
+        updateEditButtonState()
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
